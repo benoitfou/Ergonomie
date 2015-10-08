1a74ebe153ccbc4a995f08c81ba5d71dc4fb41ec
Benoitfouc <benoit.96@hotmail.fr>
2015-10-08 15:11:41 +0200

    param css chrono

--- chronometre.php
+++ chronometre.php
@@ -9,17 +9,6 @@
 	<link rel="stylesheet" href="css/themes/default/jquery.mobile-1.4.5.min.css">
 	<script src="js/jquery.js"></script>
 	<script src="js/jquery.mobile-1.4.5.min.js"></script>
-<style>
-h1, input{
-	text-align: center;
-}
-#Text_1{
-	text-align: center;
-}
-#footer{
-	text-align: center;
-}
-</style>
 </head>
 <body>
 	<div data-role="page">
